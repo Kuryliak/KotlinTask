d70e76a965dbb53240fd4a5853d5813e2d63dcc8
Dmitry Barannik <dmitryb.dev@gmail.com>
2021-12-27 21:05:52 +0200

    Fixed typo

--- src/main/kotlin/backend/infrastructure/config/AppModule.kt
+++ src/main/kotlin/backend/infrastructure/config/AppModule.kt
@@ -9,7 +9,7 @@ import backend.infrastructure.dao.ebean.EbeanUserDao
 @Module
 class AppModule {
     @Provides
-    fun blindDao(ebeanUserDao: EbeanUserDao): Dao<UserModel> = ebeanUserDao
+    fun bindDao(ebeanUserDao: EbeanUserDao): Dao<UserModel> = ebeanUserDao
 
     @Provides
     fun database() = DatabaseConfig.database
